c63e0847a7115b0203d3d01848a6334cf287353c
Elijah Roussos <elijah.rou@gmail.com>
2017-05-08 23:05:42 +0200

    HuffmanTreeTest: Completed Huffman Coding tests

--- test/HuffmanTreeTest.cpp
+++ test/HuffmanTreeTest.cpp
@@ -10,7 +10,8 @@ using namespace RSSELI007;
 std::string file("test.txt");
 HuffmanTree tree(file);
 
-TEST_CASE("Symbol map generation"){
+TEST_CASE("Symbol-Freq map generation", "[node, freqMap]"){
+    std::cout << std::endl << "Symbol-Freq map generation TEST" << std::endl;
     std::cout << "Checking input text size = 1" << std::endl;
     REQUIRE(tree.getInputText().size() == 1);
     std::cout << "PASSED" << std::endl;
@@ -25,8 +26,35 @@ TEST_CASE("Symbol map generation"){
     std::cout << "PASSED" << std::endl;
 }
 
-TEST_CASE("Queue generation"){
-    std::cout << "Checking root is not leaf" << std::endl;
-    REQUIRE(!tree.rootNode->leaf);
-    std::cout << "PASSED" << std::endl;
+TEST_CASE("Huffman coding", "[tree]"){
+    // tests that format is fine and priority queue worked
+    SECTION("Tree Formation"){
+        std::cout << std::endl << "Tree generation TEST" << std::endl;
+        std::cout << "Checking root is not leaf" << std::endl;
+        REQUIRE(!tree.rootNode->leaf);
+        std::cout << "PASSED" << std::endl;
+        std::cout << "Checking root frequency = total frequency" << std::endl;
+        REQUIRE(tree.rootNode->frequency == 14);
+        std::cout << "PASSED" << std::endl;
+    }
+    SECTION("Code Assignment"){
+        // tests that tree is formed correctly with the right codes
+        std::cout << std::endl << "Code generation TEST" << std::endl;
+        std::cout << "Checking code table is non-empty" << std::endl;
+        REQUIRE(!tree.getCodeMap().empty());
+        std::cout << "PASSED" << std::endl;
+        std::cout << "Checking code table is corrct size" << std::endl;
+        REQUIRE(tree.getCodeMap().size() == 8);
+        std::cout << "PASSED" << std::endl;
+        std::cout << "Checking codes are correct" << std::endl;
+        REQUIRE(tree.getCodeMap()[' '] == "00");
+        REQUIRE(tree.getCodeMap()['s'] == "01");
+        REQUIRE(tree.getCodeMap()['t'] == "101");
+        REQUIRE(tree.getCodeMap()['h'] == "1000");
+        REQUIRE(tree.getCodeMap()['T'] == "1001");
+        REQUIRE(tree.getCodeMap()['i'] == "110");
+        REQUIRE(tree.getCodeMap()['e'] == "1110");
+        REQUIRE(tree.getCodeMap()['a'] == "1111");
+        std::cout << "PASSED" << std::endl;
+    }
 }
